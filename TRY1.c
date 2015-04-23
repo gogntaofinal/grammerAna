2db7ab866dbabb30c462b526db347f925b5f0eb0
龚涛 <GTooo1@ZZUGTooo1.local>
2015-04-23 23:14:40 +0800

    补充了一个关于first集的求法

--- TRY1.c
+++ TRY1.c
@@ -1,3 +1,18 @@
+/**
+ ==========================================================
+ 求first集的算法思想：
+ ==========================================================
+ 如果产生式右部第一个字符为终结符，则将其计入左部first集
+ 如果产生式右部第一个字符为非终结符
+ ->求该非终结符的first集
+ ->将该非终结符的非$first集计入左部的first集
+	->若存在$，则将指向产生式的指针右移
+	->若不存在$，则停止遍历该产生式，进入下一个产生式
+	->若已经到达产生式的最右部的非终结符，则将$加入左部的first集
+ 处理数组中重复的first集中的终结符
+ ===========================================================
+ */
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -56,21 +71,73 @@ int getAllSymbol(){
 }
 */
 
-int isTer(){
+int isTer(char ch){
     int i = 0, len = strlen(terSymbol);
-    int tag;
     for(i = 0; i < len; ++i){
-    
-    } 
+        if(ch == terSymbol[i]){
+            return 1;
+        }
+    }
+    return 0;
 }
-int findFirst(){
-    
+
+int isNoTer(char ch){
+    int i = 0, len = strlen(non_ter);
+    for(i = 0; i < len; ++i){
+        if(ch == non_ter){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int findFirst(char target){
+    int i = 0;      //用来标记全体产生式的遍历
+    int j = 0;      //用来标记打个产生式的遍历
+    int lens = 0;   //用来记录整个产生式的长度
+    int lenf = 0;   //用来记录first集的长度
+    int tag = 0, tag1 = 0;    //用来表示是否存在$
+    int seti = isNoTer(target);   //用来标示是哪个非终结符的first集
+    int k = 0;      //用来表示first集的遍历
+    for(i = 0; i < pro_num; ++i){
+        strcpy(ele, terSymbol[i].formula);
+        lens = strlen(ele);
+        if(ele[0] == target){
+            for(j = 3; j < lens; ++j){
+                if(isTer(ele[j])){
+                    firstSET[seti][strlen[firstSET[seti]]] = ele[j];
+                    break;
+                }
+                else{
+                    findFIRST(ele[j]);
+                    lenf = strlen(firstSET[isNoTer(ele[j])]);
+                    for(k = 0; k < lenf; ++k){
+                        if(firstSET[isNoTer(ele[j])][k] == '$'){
+                            tag1 ++;
+                        }else{
+                            firstSET[seti][strlen[firstSET[seti]]] = firstSET[isNoTer(ele[j])][k];
+                        }
+                    }
+                    if(tag1 == 0){
+                        break;
+                    }else{
+                        tag += tag1;
+                        tag1 = 0;
+                    }
+                }
+            }
+            if(tag == lens - 3){
+                firstSET[seti][strlen[firstSET[seti]]] = '$';
+            }
+        }
+    }
+    return 0;
 }
 
 int main(){
     
-    freopen("product.txt", "r", stdin);
-    freopen("output.txt", "a", stdout);
+//    freopen("product.txt", "r", stdin);
+//    freopen("output.txt", "a", stdout);
     initana();
     strcpy(terSymbol, "ABEFT");
     strcpy(non_ter, "$*i()");
